c8aa8bbb7dc73f036744f8d90f73785897b347e1
YoshinobuShibata <web.yoshinobu@gmail.com>
2023-02-17 18:18:09 +0900

    Orderテーブルに情報を登録できるように設定

---
+++ src/app/Models/Orders.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Orders extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'order_total_amount',
+        'order_total_number',
+    ];
+}
