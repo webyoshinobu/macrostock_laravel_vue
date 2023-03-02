578d7b30521b630e5d0489fdd4083258059e1b21
YoshinobuShibata <web.yoshinobu@gmail.com>
2023-03-02 15:57:04 +0900

    メディアクエリ実装前の念の為の状態保存

--- src/resources/js/util.ts
+++ src/resources/js/util.ts
@@ -20,6 +20,9 @@ export function getCookieValue (searchKey:String) {
     return val
 }
 
+/**
+ * ステータスコード
+ */
 export const OK = 200
 export const CREATED = 201
 export const INTERNAL_SERVER_ERROR = 500
